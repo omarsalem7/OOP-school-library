528d19bcaf1eebfdf33d83eee6655717c266f9c7
omarsalem7 <omarsalem0721@gmail.com>
2022-04-11 17:06:56 +0200

    Apply changes from code review

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require './person'
 
 class Student < Person
-  def initialize(classroom, age, name, parent_permission)
+  def initialize(classroom, age, name, parent_permission=true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
